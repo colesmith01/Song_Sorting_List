6a4cffe933d76ec830d68e690763d5a9e9cef1c3
Cole <cole.smith@xtra.co.nz>
2022-07-12 16:49:18 +1200

    added music to gitignore, finished directory_iterator

--- test.cpp
+++ test.cpp
@@ -9,19 +9,20 @@ using std::endl;
 
 
 void directory_iterator(std::string path){
-    for (const auto &entry : std::filesystem::directory_iterator(path)){
-      if (!entry.path().has_extension())
-        directory_iterator(entry.path().string());
-      else
-        cout << entry.path() << endl;
-    }
+  for (const auto &entry : std::filesystem::directory_iterator(path)){
+    if (std::filesystem::is_directory(entry.path()))
+      directory_iterator(entry.path().string());
+
+    else if (entry.path().has_extension())
+      cout << entry.path() << endl;
+  }
 }
 
 int main() {
-    std::string path = "C:\\Users\\user\\Desktop\\Song_Sorting_Program\\Song_Test_Data";
-    directory_iterator(path);
-    /*
-    for (const auto &entry : std::filesystem::directory_iterator(path))
-        std::cout << entry.path() << std::endl;
-    */
+  std::string path = "C:\\Users\\user\\Desktop\\Song_Sorting_Program\\Song_Test_Data";
+  directory_iterator(path);
+  /*
+  for (const auto &entry : std::filesystem::directory_iterator(path))
+      std::cout << entry.path() << std::endl;
+  */
 }
