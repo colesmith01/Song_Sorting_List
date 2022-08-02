5c0d0b68810806efd6fa21294713d09c5495bbef
Cole <cole.smith@xtra.co.nz>
2022-08-02 22:16:32 +1200

    Character type interfering with iterator

--- main.cpp
+++ main.cpp
@@ -3,80 +3,51 @@
 #include <cstring>
 #include <filesystem>
 
-#include "Node.cpp"
-
 using std::cout;
 using std::endl;
 
+int i = 0;
+int test;
+
 void directory_iterator(std::string root_path){
-	
-	MP3 file;
-	Node* currentSong = new Node();
-
 	for (const auto &entry : std::filesystem::directory_iterator(root_path)){
-		if (std::filesystem::is_directory(entry.path()))
+		if (std::filesystem::is_directory(entry.path())){
+			cout << entry.path() << endl;
 			directory_iterator(entry.path().string());
-
+			cout << entry.path() << endl;
+		}
 		else if (entry.path().has_extension()){
-			ID3::load();
-
-			Node* currentSong = new Node();
-			currentSong->setPath(entry.path().string());
-
 			const int path_length = entry.path().string().length();
 
 			char char_path[path_length];
 			
 			strcpy(char_path, entry.path().string().c_str());
-
-			file.read(char_path);
-
-
-			currentSong->setSong(file);
-
-			ID3::ID3v1 id3v1 = file.getID3v1();
 			
-			cout << "Path: " << entry.path() << endl;
+			i++;
 
-			if (entry.path().extension() != ".mp3")
+			if (entry.path().extension() != ".mp3"){
+				cout << "Path: " << entry.path() << endl;
 				cout << "not an mp3: " << entry.path().extension() << endl << endl;
-			else if(!file.hasID3v1)
-				cout << "no id3 data detected" << endl << endl;
-			else 
-				cout << "Title: " << id3v1.title << endl << endl;
-			
-			delete currentSong;
-			ID3::clear();
+				test = 0;
+			}
+			else{
+				//search for high bitrate
+			}
+
+			cout << "test " << test++ << ": " << entry.path() << endl;
 		}
 	}
+
+	
 
 	
 }
 
 int main() {
 	
-	std::string path = "Song_Test_Data";
-  	directory_iterator(path);
-	
-	
-	/*
-	ID3::load();
-	
-	MP3 file;
-	Node* currentSong = new Node();
-
-	file.read("Song_Test_Data\\Document One\\UnknownAlbum\\Document One - Bump the Sound.mp3");
-	
-	currentSong->setSong(file);
-	
-	ID3::ID3v1 id3v1 = file.getID3v1();
-	
-	cout << "Title: " << id3v1.title << endl << endl;
-
-	ID3::clear();
-
-	delete currentSong;
-	*/
+	std::string path = "D:\\Rekordbox USB Backup\\Contents";
+  	//directory_iterator(path);
+	cout << "ō" << endl;
 
 	return 0;
 }
