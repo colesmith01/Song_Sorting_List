969c875de7dbd48d4dd99457afdd975372f870f6
Cole <cole.smith@xtra.co.nz>
2022-08-03 14:26:16 +1200

    added check for illegal characters (need to store illegal directories for reference)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <cstring>
 #include <filesystem>
+#include <sstream>
 
 using std::cout;
 using std::endl;
@@ -9,11 +10,29 @@ using std::endl;
 int i = 0;
 int test;
 
+std::string to_hex(char ch) {
+    std::ostringstream b;
+    b << "\\x" << std::setfill('0') << std::setw(2) << std::setprecision(2)
+        << std::hex << static_cast<unsigned int>(ch & 0xff);
+    return b.str();
+}
+
 void directory_iterator(std::string root_path){
+
+	//refine code
+	for (int i = 0; i < stringTest.length(); i++) {
+		if (stringTest[i] < 0)
+			cout << "Skipped" << endl;
+			return;
+	}
+
+	
 	for (const auto &entry : std::filesystem::directory_iterator(root_path)){
 		if (std::filesystem::is_directory(entry.path())){
 			cout << entry.path() << endl;
+
 			directory_iterator(entry.path().string());
+			
 			cout << entry.path() << endl;
 		}
 		else if (entry.path().has_extension()){
@@ -44,10 +63,16 @@ void directory_iterator(std::string root_path){
 }
 
 int main() {
-	
+	/*
 	std::string path = "D:\\Rekordbox USB Backup\\Contents";
-  	//directory_iterator(path);
-	cout << "ō" << endl;
+  	directory_iterator(path);
+	 */
+	std::string stringTest = "D:\\Rekordbox USB Backup\\Contents\\Ajja; Simiantics\\Kenshō";
 
+
+	
+
+	//cout << (int)stringTest[stringTest.length()-1] << endl;  
+	
 	return 0;
-}
+}  
