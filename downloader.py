e8f29d2c7612ecb85d6a23c046c4c5d1bc2b2417
Cole <cole.smith@xtra.co.nz>
2022-09-24 02:46:35 +1200

    Python Directory Scraper Functional

--- downloader.py
+++ downloader.py
@@ -1,23 +1,42 @@
 import os
+import sys
+
+from regex import S
+import eyed3
+from sympy import AlgebraicNumber
+
+def directory_scraper(path):
+    for entry in os.scandir(path):
+        if entry.is_dir():
+                directory_scraper(entry.path)
+        elif entry.is_file():
+                filename, file_extension = os.path.splitext(entry.path)
+
+                typ = 'file'
+                if (file_extension == ".mp3"):
+                    audioFile = eyed3.load(entry.path)
+                    tag = audioFile.tag
+                    
+                    if(type(tag.artist).__name__ == 'str'):
+                        print('Artist: ' + tag.artist)
+                    if(type(tag.album).__name__ == 'str'):
+                        print('Album: ' + tag.album)
+                    if(type(tag.title).__name__ == 'str'):
+                        print('Title: ' + tag.title)
+        elif entry.is_symlink():
+                typ = 'link'
+        else:
+                typ = 'unknown'
+    
+        #print('{name} {typ}'.format(
+        #     name=entry.name,
+        #     typ=typ,
+        #))
+
+sys.setrecursionlimit(5000)
+
 root = 'D:\\Rekordbox USB Backup\\Contents'
-for entry in os.scandir(root):
-   if entry.is_dir():
-       typ = 'dir'
-   elif entry.is_file():
-       typ = 'file'
-   elif entry.is_symlink():
-       typ = 'link'
-   else:
-       typ = 'unknown'
-   print('{name} {typ}'.format(
-       name=entry.name,
-       typ=typ,
-   ))
-
-
-
-
-
+directory_scraper(root)
 
 
 
