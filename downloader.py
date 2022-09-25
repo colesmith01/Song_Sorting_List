2e313a957eaaaea5abc7432a27bce0f4a96370ba
Cole <cole.smith@xtra.co.nz>
2022-09-25 19:30:19 +1300

    Invalid date error on eyed3.core.load

--- downloader.py
+++ downloader.py
@@ -1,42 +1,49 @@
+from array import array
 import os
 import sys
+import numpy as np
 
 from regex import S
 import eyed3
 from sympy import AlgebraicNumber
 
-def directory_scraper(path):
+def directory_scraper(path, songTags:np.array):    
     for entry in os.scandir(path):
         if entry.is_dir():
-                directory_scraper(entry.path)
+                directory_scraper(entry.path, songTags)
         elif entry.is_file():
                 filename, file_extension = os.path.splitext(entry.path)
 
                 typ = 'file'
                 if (file_extension == ".mp3"):
-                    audioFile = eyed3.load(entry.path)
+                    audioFile = eyed3.core.load(entry.path)
                     tag = audioFile.tag
-                    
-                    if(type(tag.artist).__name__ == 'str'):
-                        print('Artist: ' + tag.artist)
-                    if(type(tag.album).__name__ == 'str'):
-                        print('Album: ' + tag.album)
-                    if(type(tag.title).__name__ == 'str'):
-                        print('Title: ' + tag.title)
+
+                    np.append(songTags, tag)
+                    #songTags.append(tag)
         elif entry.is_symlink():
                 typ = 'link'
         else:
                 typ = 'unknown'
-    
+    return songTags
         #print('{name} {typ}'.format(
         #     name=entry.name,
         #     typ=typ,
         #))
 
-sys.setrecursionlimit(5000)
 
 root = 'D:\\Rekordbox USB Backup\\Contents'
-directory_scraper(root)
+mp3tags = np.array([])
+directory_scraper(root, mp3tags)
+
+
+for tag in mp3tags:
+    if(type(tag.artist).__name__ == 'str'):
+        print('Artist: ' + tag.artist)
+    if(type(tag.album).__name__ == 'str'):
+        print('Album: ' + tag.album)
+    if(type(tag.title).__name__ == 'str'):
+        print('Title: ' + tag.title)
 
 
 
